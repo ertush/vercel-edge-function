3583453898e8a62190c8f9b64280f9b5e640435e
eric <erotush77@gmail.com>
2023-05-12 15:27:16 +0300

    First Commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,11 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  name: string
-}
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+import { NextRequest, NextResponse } from 'next/server';
+ 
+export const config = {
+  runtime: 'edge',
+};
+ 
+export default (request: NextRequest) => {
+  return NextResponse.json({
+    name: `Hello, from ${request.url} I'm now an Edge Function!`,
+  });
+};
